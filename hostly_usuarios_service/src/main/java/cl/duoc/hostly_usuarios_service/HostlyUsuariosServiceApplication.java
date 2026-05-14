b4a275fa7034092fef4faeff9f141a81cd25df01
soofigonb <sofiapazgonb@gmail.com>
2026-05-14 01:41:58 -0400

    Inicializando proyecto Hostly Pets

---
+++ hostly_usuarios_service/src/main/java/cl/duoc/hostly_usuarios_service/HostlyUsuariosServiceApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.hostly_usuarios_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostlyUsuariosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostlyUsuariosServiceApplication.class, args);
+	}
+
+}
