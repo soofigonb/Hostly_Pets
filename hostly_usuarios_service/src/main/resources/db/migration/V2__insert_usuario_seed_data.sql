478e2e233b351b1ffa4b62098440dfbb4a6208ea
soofigonb <sofiapazgonb@gmail.com>
2026-05-14 02:49:26 -0400

    feat: agregando migraciones Flyway y datos iniciales de usuarios-service

---
+++ hostly_usuarios_service/src/main/resources/db/migration/V2__insert_usuario_seed_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO db_usuario.usuario
+(nombre, apellido, email, telefono, password)
+VALUES
+('Sofia', 'Gonzalez', 'sofia@test.com', '123456789', 'admin123'),
+('Nitsi', 'Sepulveda', 'nitsi@test.com', '987654321', 'cliente123');
