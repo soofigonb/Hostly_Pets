09ed5456667980813f89963d6c9932fa58d306f9
soofigonb <sofiapazgonb@gmail.com>
2026-05-14 03:55:47 -0400

    feat: agregando estructura inicial de propiedades-service

---
+++ hostly_propiedades_service/src/main/java/cl/duoc/hostly_propiedades_service/HostlyPropiedadesServiceApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.hostly_propiedades_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostlyPropiedadesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostlyPropiedadesServiceApplication.class, args);
+	}
+
+}
