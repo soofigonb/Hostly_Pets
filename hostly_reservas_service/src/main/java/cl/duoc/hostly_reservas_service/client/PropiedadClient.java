527b017f763f56627afbd5ec5f23e4597b55e0b3
Nitsi <nitsisepulvedam.5@gmail.com>
2026-06-16 20:24:07 -0400

    nitsi:cambios localhost a 8080 en clients

--- hostly_reservas_service/src/main/java/cl/duoc/hostly_reservas_service/client/PropiedadClient.java
+++ hostly_reservas_service/src/main/java/cl/duoc/hostly_reservas_service/client/PropiedadClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "hostly-propiedades-service", url = "http://localhost:8081/api/v1/propiedades")
+@FeignClient(name = "hostly-propiedades-service", url = "http://localhost:8080/api/v1/propiedades")
 public interface PropiedadClient {
 
     @GetMapping("/{id}")
